fc3f1ac6d57ffde997093ae9438c8e47ee1f5b6c
guessi <guessi@gmail.com>
2021-12-07 18:13:43 +0800

    feat(iam): basic examples for managing IAM with cdk

---
+++ iam-management/lib/iam-management-stack.ts
@@ -0,0 +1,74 @@
+import * as cdk from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam';
+
+export class IamManagementStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create IAM User
+    const sampleUser = new iam.User(this, 'SampleUser');
+
+    // Attach Managed Policy to IAM User
+    sampleUser.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ReadOnlyAccess"),
+    );
+
+    // Attach Customized Policy to IAM User
+    const userDefinedInlinePolicy = new iam.Policy(this, 'ec2-managed-tags', {
+      statements: [
+        new iam.PolicyStatement({
+          resources: ['*'],
+          actions: [
+            'ec2:DeleteTags',
+            'ec2:CreateTags',
+          ],
+          effect: iam.Effect.ALLOW,
+        }),
+      ],
+    });
+    sampleUser.attachInlinePolicy(userDefinedInlinePolicy);
+
+    // Create IAM Group
+    const sampleGroup = new iam.Group(this, 'SampleGroup');
+
+    // Attach Managed Policy to IAM Group
+    sampleGroup.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("CloudWatchReadOnlyAccess"),
+    );
+
+    // Attach Customized Policy to IAM User
+    const groupDefinedInlinePolicy = new iam.Policy(this, 'allow-list-all-buckets', {
+      statements: [
+        new iam.PolicyStatement({
+          resources: ['*'],
+          actions: [
+            's3:ListAllMyBuckets',
+          ],
+          effect: iam.Effect.ALLOW,
+        }),
+      ],
+    });
+    sampleGroup.attachInlinePolicy(groupDefinedInlinePolicy);
+
+    // Add IAM User to IAM Group
+    sampleGroup.addUser(sampleUser);
+
+    // Create IAM Role
+    const sampleRole = new iam.Role(this, 'SampleRole', {
+      assumedBy: new iam.ServicePrincipal('sns.amazonaws.com'),
+    });
+
+    // Attach Managed Policy to IAM Role
+    sampleRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AWSLambda_ReadOnlyAccess"),
+    );
+
+    // Attach Customized Policy to IAM Role
+    sampleRole.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: [
+        'lambda:InvokeFunction',
+      ],
+    }));
+  }
+}
