fc5e79395e4a0d15569efcd261db470fea59f9c4
guessi <guessi@gmail.com>
2023-01-15 00:38:01 +0800

    Bump default EKS cluster version to 1.24

--- eks-basic/lib/eks-basic-stack.ts
+++ eks-basic/lib/eks-basic-stack.ts
@@ -3,7 +3,7 @@ import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as eks from "aws-cdk-lib/aws-eks";
 import * as iam from "aws-cdk-lib/aws-iam";
-import { KubectlV23Layer } from "@aws-cdk/lambda-layer-kubectl-v23";
+import { KubectlV24Layer } from "@aws-cdk/lambda-layer-kubectl-v24";
 
 export class EksBasicStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,9 +11,9 @@ export class EksBasicStack extends cdk.Stack {
 
     const cluster = new eks.Cluster(this, "cdk-eks-cluster", {
       clusterName: "cdk-eks-cluster",
-      version: eks.KubernetesVersion.V1_23,
+      version: eks.KubernetesVersion.V1_24,
       endpointAccess: eks.EndpointAccess.PUBLIC_AND_PRIVATE,
-      kubectlLayer: new KubectlV23Layer(this, "kubectl"),
+      kubectlLayer: new KubectlV24Layer(this, "kubectl"),
       vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }],
       serviceIpv4Cidr: "172.20.0.0/16",
       defaultCapacity: 0,
@@ -31,10 +31,17 @@ export class EksBasicStack extends cdk.Stack {
       outputConfigCommand: true,
     });
 
+    const instanceTypes = [
+      new ec2.InstanceType("t3a.small"),
+      new ec2.InstanceType("t3a.medium"),
+      new ec2.InstanceType("t3.small"),
+      new ec2.InstanceType("t3.medium"),
+    ];
+
     cluster.addNodegroupCapacity("mng-1", {
       nodegroupName: "mng-1",
       amiType: eks.NodegroupAmiType.AL2_X86_64,
-      instanceTypes: [new ec2.InstanceType("t3.small")],
+      instanceTypes: instanceTypes,
       desiredSize: 2,
       minSize: 2,
       maxSize: 5,
