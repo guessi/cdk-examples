e0aa81b37f7080e5ce77b8c66451b8baa1c928ac
guessi <guessi@gmail.com>
2023-12-23 00:07:20 +0800

    chore(eks): bump version for vpc-cni v1.15.5-eksbuild.1

--- eks-basic/lib/eks-basic-stack.ts
+++ eks-basic/lib/eks-basic-stack.ts
@@ -193,12 +193,12 @@ export class EksBasicStack extends cdk.Stack {
 
     // vpc-cni
     const vpcCniVersionMap: Map<eks.KubernetesVersion, string> = new Map([
-      [eks.KubernetesVersion.V1_28, "v1.15.4-eksbuild.1"],
-      [eks.KubernetesVersion.V1_27, "v1.15.4-eksbuild.1"],
-      [eks.KubernetesVersion.V1_26, "v1.15.4-eksbuild.1"],
-      [eks.KubernetesVersion.V1_25, "v1.15.4-eksbuild.1"],
-      [eks.KubernetesVersion.V1_24, "v1.15.4-eksbuild.1"],
-      [eks.KubernetesVersion.V1_23, "v1.15.4-eksbuild.1"],
+      [eks.KubernetesVersion.V1_28, "v1.15.5-eksbuild.1"],
+      [eks.KubernetesVersion.V1_27, "v1.15.5-eksbuild.1"],
+      [eks.KubernetesVersion.V1_26, "v1.15.5-eksbuild.1"],
+      [eks.KubernetesVersion.V1_25, "v1.15.5-eksbuild.1"],
+      [eks.KubernetesVersion.V1_24, "v1.15.5-eksbuild.1"],
+      [eks.KubernetesVersion.V1_23, "v1.15.5-eksbuild.1"],
     ]);
 
     const cfnAddonVpcCni = new eks.CfnAddon(this, "cfnAddonVpcCni", {
