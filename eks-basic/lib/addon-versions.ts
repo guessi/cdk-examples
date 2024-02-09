fdda0a10b306ebf7454aac49219b36731f706f4c
guessi <guessi@gmail.com>
2024-02-10 02:20:04 +0800

    chore(eks): bump addons version and drop Amazon EKS 1.23 support

--- eks-basic/lib/addon-versions.ts
+++ eks-basic/lib/addon-versions.ts
@@ -5,29 +5,26 @@ export const kubeProxyVersionMap: Map<eks.KubernetesVersion, string> = new Map([
   [eks.KubernetesVersion.V1_28, "v1.28.4-eksbuild.4"],
   [eks.KubernetesVersion.V1_27, "v1.27.8-eksbuild.4"],
   [eks.KubernetesVersion.V1_26, "v1.26.11-eksbuild.4"],
-  [eks.KubernetesVersion.V1_25, "v1.25.16-eksbuild.1"],
-  [eks.KubernetesVersion.V1_24, "v1.24.17-eksbuild.4"],
-  [eks.KubernetesVersion.V1_23, "v1.23.17-eksbuild.5"],
+  [eks.KubernetesVersion.V1_25, "v1.25.16-eksbuild.2"],
+  [eks.KubernetesVersion.V1_24, "v1.24.17-eksbuild.8"],
 ]);
 
 // coredns
 export const coreDnsVersionMap: Map<eks.KubernetesVersion, string> = new Map([
-  [eks.KubernetesVersion.V1_28, "v1.10.1-eksbuild.6"],
-  [eks.KubernetesVersion.V1_27, "v1.10.1-eksbuild.6"],
-  [eks.KubernetesVersion.V1_26, "v1.9.3-eksbuild.10"],
-  [eks.KubernetesVersion.V1_25, "v1.9.3-eksbuild.10"],
-  [eks.KubernetesVersion.V1_24, "v1.9.3-eksbuild.10"],
-  [eks.KubernetesVersion.V1_23, "v1.8.7-eksbuild.9"],
+  [eks.KubernetesVersion.V1_28, "v1.10.1-eksbuild.7"],
+  [eks.KubernetesVersion.V1_27, "v1.10.1-eksbuild.7"],
+  [eks.KubernetesVersion.V1_26, "v1.9.3-eksbuild.11"],
+  [eks.KubernetesVersion.V1_25, "v1.9.3-eksbuild.11"],
+  [eks.KubernetesVersion.V1_24, "v1.9.3-eksbuild.11"],
 ]);
 
 // vpc-cni
 export const vpcCniVersionMap: Map<eks.KubernetesVersion, string> = new Map([
-  [eks.KubernetesVersion.V1_28, "v1.16.0-eksbuild.1"],
-  [eks.KubernetesVersion.V1_27, "v1.16.0-eksbuild.1"],
-  [eks.KubernetesVersion.V1_26, "v1.16.0-eksbuild.1"],
-  [eks.KubernetesVersion.V1_25, "v1.16.0-eksbuild.1"],
-  [eks.KubernetesVersion.V1_24, "v1.16.0-eksbuild.1"],
-  [eks.KubernetesVersion.V1_23, "v1.15.5-eksbuild.1"],
+  [eks.KubernetesVersion.V1_28, "v1.16.2-eksbuild.1"],
+  [eks.KubernetesVersion.V1_27, "v1.16.2-eksbuild.1"],
+  [eks.KubernetesVersion.V1_26, "v1.16.2-eksbuild.1"],
+  [eks.KubernetesVersion.V1_25, "v1.16.2-eksbuild.1"],
+  [eks.KubernetesVersion.V1_24, "v1.16.2-eksbuild.1"],
 ]);
 
 // eks-pod-identity-agent
@@ -38,17 +35,13 @@ export const eksPodIdentityAgentVersionMap: Map<eks.KubernetesVersion, string> =
     [eks.KubernetesVersion.V1_26, "v1.1.0-eksbuild.1"],
     [eks.KubernetesVersion.V1_25, "v1.1.0-eksbuild.1"],
     [eks.KubernetesVersion.V1_24, "v1.1.0-eksbuild.1"],
-    // Only Amazon EKS 1.24+ are supported.
-    // - https://docs.aws.amazon.com/eks/latest/userguide/pod-identities.html#pod-id-considerations
-    // - https://aws.amazon.com/blogs/aws/amazon-eks-pod-identity-simplifies-iam-permissions-for-applications-on-amazon-eks-clusters/
   ]);
 
 // aws-ebs-csi-driver
 export const ebsCsiVersionMap: Map<eks.KubernetesVersion, string> = new Map([
-  [eks.KubernetesVersion.V1_28, "v1.26.1-eksbuild.1"],
-  [eks.KubernetesVersion.V1_27, "v1.26.1-eksbuild.1"],
-  [eks.KubernetesVersion.V1_26, "v1.26.1-eksbuild.1"],
-  [eks.KubernetesVersion.V1_25, "v1.26.1-eksbuild.1"],
-  [eks.KubernetesVersion.V1_24, "v1.26.1-eksbuild.1"],
-  [eks.KubernetesVersion.V1_23, "v1.26.1-eksbuild.1"],
+  [eks.KubernetesVersion.V1_28, "v1.27.0-eksbuild.1"],
+  [eks.KubernetesVersion.V1_27, "v1.27.0-eksbuild.1"],
+  [eks.KubernetesVersion.V1_26, "v1.27.0-eksbuild.1"],
+  [eks.KubernetesVersion.V1_25, "v1.27.0-eksbuild.1"],
+  [eks.KubernetesVersion.V1_24, "v1.27.0-eksbuild.1"],
 ]);
