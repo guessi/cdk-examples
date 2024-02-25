c8076be7de7bd28a883d3288baa739f47e0ea03c
guessi <guessi@gmail.com>
2024-02-25 10:55:14 +0800

    chore(eks): bump addons version

--- eks-basic/lib/addon-versions.ts
+++ eks-basic/lib/addon-versions.ts
@@ -2,11 +2,11 @@ import * as eks from "aws-cdk-lib/aws-eks";
 
 // kube-proxy
 export const kubeProxyVersionMap: Map<eks.KubernetesVersion, string> = new Map([
-  [eks.KubernetesVersion.V1_29, "v1.29.0-eksbuild.3"],
-  [eks.KubernetesVersion.V1_28, "v1.28.4-eksbuild.4"],
-  [eks.KubernetesVersion.V1_27, "v1.27.8-eksbuild.4"],
-  [eks.KubernetesVersion.V1_26, "v1.26.11-eksbuild.4"],
-  [eks.KubernetesVersion.V1_25, "v1.25.16-eksbuild.2"],
+  [eks.KubernetesVersion.V1_29, "v1.29.1-eksbuild.2"],
+  [eks.KubernetesVersion.V1_28, "v1.28.6-eksbuild.2"],
+  [eks.KubernetesVersion.V1_27, "v1.27.10-eksbuild.2"],
+  [eks.KubernetesVersion.V1_26, "v1.26.13-eksbuild.2"],
+  [eks.KubernetesVersion.V1_25, "v1.25.16-eksbuild.3"],
   [eks.KubernetesVersion.V1_24, "v1.24.17-eksbuild.8"],
 ]);
 
@@ -22,31 +22,31 @@ export const coreDnsVersionMap: Map<eks.KubernetesVersion, string> = new Map([
 
 // vpc-cni
 export const vpcCniVersionMap: Map<eks.KubernetesVersion, string> = new Map([
-  [eks.KubernetesVersion.V1_29, "v1.16.2-eksbuild.1"],
-  [eks.KubernetesVersion.V1_28, "v1.16.2-eksbuild.1"],
-  [eks.KubernetesVersion.V1_27, "v1.16.2-eksbuild.1"],
-  [eks.KubernetesVersion.V1_26, "v1.16.2-eksbuild.1"],
-  [eks.KubernetesVersion.V1_25, "v1.16.2-eksbuild.1"],
-  [eks.KubernetesVersion.V1_24, "v1.16.2-eksbuild.1"],
+  [eks.KubernetesVersion.V1_29, "v1.16.3-eksbuild.2"],
+  [eks.KubernetesVersion.V1_28, "v1.16.3-eksbuild.2"],
+  [eks.KubernetesVersion.V1_27, "v1.16.3-eksbuild.2"],
+  [eks.KubernetesVersion.V1_26, "v1.16.3-eksbuild.2"],
+  [eks.KubernetesVersion.V1_25, "v1.16.3-eksbuild.2"],
+  [eks.KubernetesVersion.V1_24, "v1.16.3-eksbuild.2"],
 ]);
 
 // eks-pod-identity-agent
 export const eksPodIdentityAgentVersionMap: Map<eks.KubernetesVersion, string> =
   new Map([
-    [eks.KubernetesVersion.V1_29, "v1.1.0-eksbuild.1"],
-    [eks.KubernetesVersion.V1_28, "v1.1.0-eksbuild.1"],
-    [eks.KubernetesVersion.V1_27, "v1.1.0-eksbuild.1"],
-    [eks.KubernetesVersion.V1_26, "v1.1.0-eksbuild.1"],
-    [eks.KubernetesVersion.V1_25, "v1.1.0-eksbuild.1"],
-    [eks.KubernetesVersion.V1_24, "v1.1.0-eksbuild.1"],
+    [eks.KubernetesVersion.V1_29, "v1.2.0-eksbuild.1"],
+    [eks.KubernetesVersion.V1_28, "v1.2.0-eksbuild.1"],
+    [eks.KubernetesVersion.V1_27, "v1.2.0-eksbuild.1"],
+    [eks.KubernetesVersion.V1_26, "v1.2.0-eksbuild.1"],
+    [eks.KubernetesVersion.V1_25, "v1.2.0-eksbuild.1"],
+    [eks.KubernetesVersion.V1_24, "v1.2.0-eksbuild.1"],
   ]);
 
 // aws-ebs-csi-driver
 export const ebsCsiVersionMap: Map<eks.KubernetesVersion, string> = new Map([
-  [eks.KubernetesVersion.V1_29, "v1.27.0-eksbuild.1"],
-  [eks.KubernetesVersion.V1_28, "v1.27.0-eksbuild.1"],
-  [eks.KubernetesVersion.V1_27, "v1.27.0-eksbuild.1"],
-  [eks.KubernetesVersion.V1_26, "v1.27.0-eksbuild.1"],
-  [eks.KubernetesVersion.V1_25, "v1.27.0-eksbuild.1"],
-  [eks.KubernetesVersion.V1_24, "v1.27.0-eksbuild.1"],
+  [eks.KubernetesVersion.V1_29, "v1.28.0-eksbuild.1"],
+  [eks.KubernetesVersion.V1_28, "v1.28.0-eksbuild.1"],
+  [eks.KubernetesVersion.V1_27, "v1.28.0-eksbuild.1"],
+  [eks.KubernetesVersion.V1_26, "v1.28.0-eksbuild.1"],
+  [eks.KubernetesVersion.V1_25, "v1.28.0-eksbuild.1"],
+  [eks.KubernetesVersion.V1_24, "v1.28.0-eksbuild.1"],
 ]);
