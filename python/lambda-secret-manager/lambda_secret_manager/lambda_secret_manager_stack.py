dfe08840ea757f6326837b67a8cbd46c52d4c3c5
guessi <guessi@gmail.com>
2024-03-23 23:55:12 +0800

    feat(lambda): sample code for lambda/secretsmanager

---
+++ python/lambda-secret-manager/lambda_secret_manager/lambda_secret_manager_stack.py
@@ -0,0 +1,38 @@
+from aws_cdk import (
+    Stack, SecretValue,
+    aws_secretsmanager as secretsmanager,
+    aws_lambda as lambda_,
+    aws_iam as iam,
+)
+
+from constructs import Construct
+
+class LambdaSecretManagerStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        ss = secretsmanager.Secret(self, "Secret",
+            secret_object_value={
+                "HIDDEN_VALUE": SecretValue.unsafe_plain_text("TOP_SECRET"),
+            }
+        )
+
+        fn = lambda_.Function(self, "Function",
+            runtime=lambda_.Runtime.PYTHON_3_12,
+            handler="app.lambda_handler",
+            code=lambda_.Code.from_asset("lambda_code"),
+            environment={
+                "HIDDEN_VALUE": ss.secret_name,
+            },
+        )
+
+        fn.add_to_role_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=[
+                'secretsmanager:GetSecretValue',
+            ],
+            resources=[
+                ss.secret_arn,
+            ],
+        ))
