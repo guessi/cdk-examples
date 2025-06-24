1a754f8e668abd3fc7168bf8e44df82e4c6a505f
guessi <guessi@gmail.com>
2025-06-25 00:01:55 +0800

    chore(base): update helper script

--- update-cdk-version.sh
+++ update-cdk-version.sh
@@ -4,14 +4,6 @@ NPM=$(which npm)
 NCU=$(which ncu)
 PIP=$(which pip3)
 
-LATEST_CDK_VERSION=$(${NPM} view aws-cdk --json | jq -r .version)
-LATEST_CDK_LIB_VERSION=$(${NPM} view aws-cdk-lib --json | jq -r .version)
-LATEST_KUBECTL_LAYER_VERSION=$(${NPM} view @aws-cdk/lambda-layer-kubectl-v32 --json | jq -r .version)
-
-echo "[debug] Latest aws-cdk Version: ${LATEST_CDK_VERSION}"
-echo "[debug] Latest aws-cdk-lib Version: ${LATEST_CDK_LIB_VERSION}"
-echo "[debug] Latest @aws-cdk/lambda-layer-kubectl-v32 Version: ${LATEST_KUBECTL_LAYER_VERSION}"
-
 $NPM install --global npm-check-updates
 
 for folder in $(find typescript -type d -depth 1); do
@@ -25,8 +17,13 @@ done
 for folder in $(find python -type d -depth 1); do
     echo "[debug] entering folder $folder"
     pushd $folder
-        cat requirements.txt | sed 's/^aws-cdk-lib==.*/aws-cdk-lib=='${LATEST_CDK_LIB_VERSION}'/g' | tee requirements.txt
-        cat requirements.txt | sed 's/^aws-cdk.lambda-layer-kubectl-v32==.*/aws-cdk.lambda-layer-kubectl-v32=='${LATEST_KUBECTL_LAYER_VERSION}'/g' | tee requirements.txt
         $PIP install -r requirements.txt -U
+        $PIP freeze | grep 'aws-cdk-lib' > requirements.txt
+
+        if [ "${folder}" = "python/eks-basic" ]; then
+          $PIP freeze | grep -E '(aws-cdk-lib|aws-cdk\.(aws-eks-v2-alpha|lambda-layer-kubectl))' > requirements.txt
+        fi
+
+        echo 'constructs>=10.0.0,<11.0.0' >> requirements.txt
     popd
 done
